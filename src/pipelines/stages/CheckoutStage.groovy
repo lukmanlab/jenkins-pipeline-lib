701884fe9db6233b7355b8bb40daba36c2ab067e
Ahmad Lukman Hakim <admin@lukmanlab.com>
2025-11-29 20:35:18 +0700

    feat: support checkout with auth

--- src/pipelines/stages/CheckoutStage.groovy
+++ src/pipelines/stages/CheckoutStage.groovy
@@ -10,11 +10,16 @@ class CheckoutStage implements PipelineStage, Serializable {
     }
 
     def execute(Map config) {
+        def userRemoteConfig = [url: config.repoUrl]
+        if (config.githubCredentialId) {
+            userRemoteConfig.credentialsId = config.githubCredentialId
+        }
+
         script.echo "Checking out code from ${config.repoUrl}"
         script.checkout([
                 $class: 'GitSCM',
                 branches: [[ name: config.branch ?: '*/main' ]],
-                userRemoteConfigs: [[ url: config.repoUrl ]]
+                userRemoteConfigs: [ userRemoteConfig ]
         ])
     }
 }

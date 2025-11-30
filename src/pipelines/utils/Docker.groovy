04d94f7cae7ae55f48575d9913a5dfa7118ec600
Ahmad Lukman Hakim <admin@lukmanlab.com>
2025-11-30 14:46:54 +0700

    feat: add default registry if not set

--- src/pipelines/utils/Docker.groovy
+++ src/pipelines/utils/Docker.groovy
@@ -3,7 +3,8 @@ package pipelines.utils
 class Docker implements Serializable {
     static String buildTag(Map config) {
         def env = Common.buildEnvironment(config)
-        def tag = "${config.dockerRegistry}/${config.registryNamespace}/${config.projectName}:${config.imageTag}-${env}"
+        def registry = config.dockerRegistry ?: 'asia-southeast2-docker.pkg.dev'
+        def tag = "${registry}/${config.registryNamespace}/${config.projectName}:${config.imageTag}-${env}"
         return tag
     }
 }
